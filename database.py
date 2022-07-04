140b0c7f3deaa53c3bd115c6ac2071996b722641
faveingpear <MPearsonindyx@gmail.com>
2022-07-03 21:01:40 -0700

    More work

--- database.py
+++ database.py
@@ -3,25 +3,25 @@ from datetime import date
 from encodings import utf_8
 from this import s
 
-class stream:
+from sympy import false, true
 
-    filepath: str
-    dateArchived: str
-    link: str
-    hash: str
-    source: int
+import json
+
+class stream(dict):
 
     def __init__(self, filepath, dateArchived, link, source, hash = None) -> None:
-        self.filepath = filepath
-        self.dateArchived = dateArchived
-        self.link = link
+        super(stream, self).__init__()
+        self.__dict__ = self
+        
+        self.__dict__["filepath"] = filepath
+        self.__dict__["dateArchived"] = dateArchived
+        self.__dict__["link"] = link
+        self.__dict__["source"] = source
 
         if hash == None:
-            self.hash = self.genHash(link)
+            self.__dict__["hash"] = str(self.genHash(link))
         else:
-            self.hash = hash
-
-        self.source = source
+            self.__dict__["hash"] = hash
 
     def genHash(self, link):
         return base64.b64encode(bytes(link, "utf_8"))
@@ -35,16 +35,20 @@ class database(dict):
         self.__dict__ = self
         self.dataBasePath = dataBasePath
 
-        try:
-            self.loadDataBase(self.dataBasePath)
-        except FileNotFoundError:
-            print("Database not found, creating a new database")
+        if self.loadDataBase(self.dataBasePath) == False:
             self.createNewDatabase(self.dataBasePath)
 
-    def loadDataBase(self, path):
-        file = open(path + "database.json", "r")
+
+    def loadDataBase(self, path) -> bool:
+        try:
+            file = open(path + "database.json", "r")
+        except FileNotFoundError:
+            print("FILE NOT FOUND ERROR")
+            self.createNewDatabase(self.dataBasePath)
         data = file.read()
         file.close()
+
+        return True
 
     def createNewDatabase(self, path):
         file = open(path + "database.json", "x")
@@ -55,9 +59,18 @@ class database(dict):
         print("Added Entry " + str(streamObject.hash) )
         self.__dict__[streamObject.hash] = streamObject
 
+    def saveEntry(self, path=None):
+
+        if path == None:
+            with open(self.dataBasePath + "database.json", "w") as outfile:
+                json.dump(self.__dict__, outfile)
+        else:
+            pass
+
 if __name__ == "__main__":
     testStream = stream("~/Projects/video.mp4", "070222", "link", 1)
     print(testStream.hash)
-    db = database("/home/mpearsonindyx/Projects/StreamArchiver/")
+    db = database("/home/faveing/Documents/gits/StreamArchiver/")
     db.addEntry(testStream)
     print(db)
+    db.saveEntry()
